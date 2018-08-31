52f8b46d2aa07d54d9024469d16c200231235837
ihorkalchenko <ihorka88@gmail.com>
2018-08-31 11:42:12 +0300

    003

---
+++ js/site.js
@@ -0,0 +1,7 @@
+// social url init
+setTimeout(() => {
+        document.querySelector('.facebook').href = 'https://www.facebook.com/garry.kalchenko';
+        document.querySelector('.linkedin').href = 'https://www.linkedin.com/in/ihor-kalchenko/';
+        document.querySelector('.github').href = 'https://github.com/ihorkalchenko';
+        document.querySelector('.envelope').href = 'mailto:ihorka88@gmail.com';
+    }, 100);
